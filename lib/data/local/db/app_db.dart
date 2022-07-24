f1f066fcf11214fd466be6373d6e2f722b9ae356
AUNG533 <aungkyawmon@gmail.com>
2022-07-24 22:15:00 +0700

    Insert Employee To Database

--- lib/data/local/db/app_db.dart
+++ lib/data/local/db/app_db.dart
@@ -1,3 +1,6 @@
+
+import 'dart:async';
+import 'dart:core';
 import 'dart:io';
 
 import 'package:drift/drift.dart';
@@ -23,4 +26,29 @@ class AppDb extends _$AppDb {
 
   @override
   int get schemaVersion => 1;
+
+  // Get the list of employees
+  Future<List<EmployeeData>> getEmployees() async {
+    return await select(employee).get();
+  }
+
+  // Get the employee by id
+  Future<EmployeeData> getEmployee(int id) async {
+    return await (select(employee)..where((tbl) => tbl.id.equals(id))).getSingle();
+  }
+
+  // update the employee
+  Future<bool> updateEmployee(EmployeeCompanion entity) async {
+    return await update(employee).replace(entity);
+  }
+
+  // insert the employee
+  Future<int> insertEmployee(EmployeeCompanion entity) async {
+    return await into(employee).insert(entity);
+  }
+
+  // delete the employee
+  Future<int> deleteEmployee(int id) async {
+    return await (delete(employee)..where((tbl) => tbl.id.equals(id))).go();
+  }
 }
