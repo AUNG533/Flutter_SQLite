98c0e5d28e50f44515628785767bd76c112038d9
AUNG533 <aungkyawmon@gmail.com>
2022-07-25 20:52:29 +0700

    Card Widget

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -50,14 +50,42 @@ class _HomeScreenState extends State<HomeScreen> {
                 itemBuilder: (context, index) {
                   final EmployeeData employee = employees[index];
                   return Card(
-                    child: Column(
-                      children: [
-                        Text(employee.id.toString()),
-                        Text(employee.userName.toString()),
-                        Text(employee.firstName.toString()),
-                        Text(employee.lastName.toString()),
-                        Text(employee.dateOfBirth.toString()),
-                      ],
+                    // color: Colors.grey.shade400,
+                    shape: const RoundedRectangleBorder(
+                      side: BorderSide(
+                        color: Colors.black26,
+                        style: BorderStyle.solid,
+                        width: 1.2,
+                      ),
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(16.0),
+                        bottomRight: Radius.circular(16.0),
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(employee.id.toString()),
+                          Text(
+                            employee.userName.toString(),
+                            style: const TextStyle(color: Colors.black),
+                          ),
+                          Text(
+                            employee.firstName.toString(),
+                            style: const TextStyle(color: Colors.black),
+                          ),
+                          Text(
+                            employee.lastName.toString(),
+                            style: const TextStyle(color: Colors.black),
+                          ),
+                          Text(
+                            employee.dateOfBirth.toString(),
+                            style: const TextStyle(color: Colors.black),
+                          ),
+                        ],
+                      ),
                     ),
                   );
                 },
