4358d12b77371a2e5a5aa6be629afda34c15c5bf
AUNG533 <aungkyawmon@gmail.com>
2022-07-26 20:58:06 +0700

    Create ChangeNotifier II

--- lib/onifier/employee_change_notifier.dart
+++ lib/onifier/employee_change_notifier.dart
@@ -13,9 +13,18 @@ class EmployeeChangeNotifier extends ChangeNotifier {
   List<EmployeeData> get employeeListFuture => _employeeListFuture;
   List<EmployeeData> _employeeListStream = [];
   List<EmployeeData> get employeeListStream => _employeeListFuture;
+  EmployeeData? _employeeData;
+  EmployeeData? get employeeData => _employeeData;
   String _error = '';
   String get error => _error;
+  bool _isAdded = false;
+  bool get isAdded => _isAdded;
+  bool _isUpdating = false;
+  bool get isUpdating => _isUpdating;
+  bool _isDeleting = false;
+  bool get isDeleting => _isDeleting;
 
+  // Get the list of employees
   void getEmployeeFuture() {
     _appDb?.getEmployees()
         .then((value) {
@@ -26,6 +35,7 @@ class EmployeeChangeNotifier extends ChangeNotifier {
     notifyListeners();
   }
 
+  // Get employee list from stream
   void getEmployeeStream() {
     _appDb?.getEmployeesStream().listen((event) {
       _employeeListStream = event;
@@ -33,5 +43,46 @@ class EmployeeChangeNotifier extends ChangeNotifier {
     notifyListeners();
   }
 
+  // Get Single Employee
+  void getSingleEmployee(int id){
+    _appDb?.getEmployee(id)
+        .then((value) {
+      _employeeData = value;
+    }).onError((error, stackTrace) {
+      _error = error.toString();
+    });
+    notifyListeners();
+  }
 
+  // Create Employee
+  void createEmployee(EmployeeCompanion entity) {
+   _appDb?.insertEmployee(entity)
+        .then((value) {
+      _isAdded = value >= 1 ? true : false;
+    }).onError((error, stackTrace) {
+      _error = error.toString();
+    });
+    notifyListeners();
+  }
+
+  // update Employee
+  void updateEmployee(EmployeeCompanion entity) {
+    _appDb?.updateEmployee(entity)
+        .then((value) {
+        _isUpdating = value;
+    }).onError((error, stackTrace) {
+      _error = error.toString();
+    });
+    notifyListeners();
+  }
+
+  // delete Employee
+  void deleteEmployee(int id) {
+    _appDb?.deleteEmployee(id).then((value) {
+      _isDeleting = value == 1 ? true : false;
+    }).onError((error, stackTrace) {
+      _error = error.toString();
+    });
+    notifyListeners();
+  }
 }
